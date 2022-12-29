e4e7473a02e3195a316f6f82681a864e709f2a36
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2022-12-29 14:17:00 +0530

    #include<stdio.h> int main() {   int w,x,y,z;   scanf("%d %d %d %d",&w,&x,&y,&z);   if(w==x||w==y||w==z||w==x+y||w==y+z||w==z+x||w==x+y+z)   printf("YES");   else   printf("NO"); }

--- Measuring_the_Object.c
+++ Measuring_the_Object.c
@@ -4,11 +4,7 @@ int main()
   int w,x,y,z;
   scanf("%d %d %d %d",&w,&x,&y,&z);
   if(w==x||w==y||w==z||w==x+y||w==y+z||w==z+x||w==x+y+z)
-  {
   printf("YES");
-  }
   else
-  {
   printf("NO");
-  }
 }
