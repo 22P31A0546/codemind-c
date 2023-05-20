4250f6a3c99e63414cc36901a43a6f6e4a991e1b
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2023-05-20 11:56:09 +0530

    #include<stdio.h> int main(){     int n,q,rem,rev=0;     scanf("%d",&n);     q=n;     while(q!=0){         rem=q%10;         q=q/10;         rev=(rev*10)+rem;     }     printf("%d",rev); }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,20 +1,12 @@
 #include<stdio.h>
-int is_palndrm(int);
-int main()
-{
-	int n;
-	scanf("%d",&n);
-	printf("%d",is_palndrm(n));	
-}
-int is_palndrm(int m)
-{
-	int q,r,s=0;
-	q=m;
-	while(q!=0)
-	{
-		r=q%10;
-		s=s*10+r;
-		q=q/10;	
-	}
-	return s;
+int main(){
+    int n,q,rem,rev=0;
+    scanf("%d",&n);
+    q=n;
+    while(q!=0){
+        rem=q%10;
+        q=q/10;
+        rev=(rev*10)+rem;
+    }
+    printf("%d",rev);
 }
