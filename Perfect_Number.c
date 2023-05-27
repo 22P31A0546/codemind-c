8da0fe9f0dda13cca7586c09a4180e6a33e794ee
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2023-05-27 16:51:09 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,s=0;     scanf("%d",&n);     for(i=1;i<n;i++) {         if(n%i==0) {             s=s+i;         }     }     if(s==n) printf("True");     else printf("False"); }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,21 +1,13 @@
 #include<stdio.h>
-int main()
-{
-    int n,i,tc=0;
+#include<math.h>
+int main() {
+    int n,i,s=0;
     scanf("%d",&n);
-    for(i=1;i<=n/2;i++)
-    {
-        if(n%i==0)
-        {
-            tc=tc+i;
+    for(i=1;i<n;i++) {
+        if(n%i==0) {
+            s=s+i;
         }
     }
-    if(tc==n)
-    {
-      printf("True");  
-    }
-    else
-    {
-       printf("False"); 
-    }
+    if(s==n) printf("True");
+    else printf("False");
 }
