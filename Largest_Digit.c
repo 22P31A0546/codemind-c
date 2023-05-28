559e33417cde0587eba6423916355c85f6110668
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2023-05-28 22:58:13 +0530

    #include<stdio.h> int main() {     int n,r,s=0;     scanf("%d",&n);     while(n>0) {         r=n%10;         n=n/10;         if(r>s) {             s=r;         }     }     printf("%d",s);     return 0; }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
-int main()
-{
-	int n,q,r,s=0,max;
-	scanf("%d",&n);
-	q=n;
-	max=0;
-	while(q!=0)
-	{
-		r=q%10;
-		if(r>max)
-		{
-			max=r;
-		}
-		q=q/10;		
-	}
-	printf("%d",max);
+int main() {
+    int n,r,s=0;
+    scanf("%d",&n);
+    while(n>0) {
+        r=n%10;
+        n=n/10;
+        if(r>s) {
+            s=r;
+        }
+    }
+    printf("%d",s);
+    return 0;
 }
