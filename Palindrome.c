b45b7cdbafc97b014c241bdbf57e472c3e05f179
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2023-05-29 21:54:48 +0530

    #include<stdio.h> int main() {     int n,r=0,re,o; scanf("%d",&n);     o=n;     while(n!=0) {         re=n%10;         r=r*10+re;         n/=10;     }     if(o==r)  printf("True");     else printf("False"); }

--- Palindrome.c
+++ Palindrome.c
@@ -1,34 +1,12 @@
 #include<stdio.h>
-int is_pal(int);
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    if(is_pal(n))
-    {
-        printf("True");
+int main() {
+    int n,r=0,re,o; scanf("%d",&n);
+    o=n; 
+    while(n!=0) {
+        re=n%10;
+        r=r*10+re;
+        n/=10;
     }
-    else
-    {
-        printf("False");
-    }
-}
-int is_pal(int m)
-{
-    int q,r,s=0;
-    q=m;
-    while(q!=0)
-    {
-        r=q%10;
-        s=s*10+r;
-        q=q/10;
-    }
-    if(s==m)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+    if(o==r)  printf("True");
+    else printf("False");
 }
