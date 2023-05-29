ffcc048188aa9f4791a15e612fc7b3b80a4c54f1
22P31A0546 <119731881+22P31A0546@users.noreply.github.com>
2023-05-29 21:45:16 +0530

    #include<stdio.h> int main() {     int n,s=0,rem;     scanf("%d",&n);     while(s!=1 && s!=4) {         s=0;         while(n!=0) {             rem=n%10;             s+=(rem*rem); n/=10;         }         n=s;     }     if(s==1) printf("True");     else printf("False"); }

--- happy_number.c
+++ happy_number.c
@@ -1,32 +1,15 @@
 #include<stdio.h>
-int main()
-{
-	int n,q,r,s=0;
-	scanf("%d",&n);
-	q=n;
-	while(q>9)
-	{
-		s=0;
-	while(q!=0)
-	{
-		r=q%10;
-		s=s+r*r;
-		q=q/10;		
-	}
-	if(s>9)
-	{
-		q=s;
-	}
-
-	}
-	if(s==1||s==7)
-	{
-		printf("True");
-	}
-	else
-	{
-		printf("False");
-	}
-	
-//	printf("%d",s);
+int main() {
+    int n,s=0,rem;
+    scanf("%d",&n);
+    while(s!=1 && s!=4) {
+        s=0;
+        while(n!=0) {
+            rem=n%10;
+            s+=(rem*rem); n/=10;
+        }
+        n=s;
+    }
+    if(s==1) printf("True");
+    else printf("False");
 }
